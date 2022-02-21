dd6a158d80e6ae925596410ec85a1651ea18e99b
Nicklas Pedersen <nick8186@zbc.dk>
2022-02-21 16:10:41 +0100

    change functions to more sensible names

--- lexer.h
+++ lexer.h
@@ -52,8 +52,8 @@ struct TokenList {
     int num;
 };
 
-void checkifeverythingsalrightbro(void);
-char const *gettokenkindname(enum TokenKind kind);
+void check_tokenkindname_array(void);
+char const *get_token_kind_name(enum TokenKind kind);
 struct TokenList lex_with_str(char *buff);
 void print_tokens(struct TokenList tkl);
 
